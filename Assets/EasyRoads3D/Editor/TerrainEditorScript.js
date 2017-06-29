9a4d3062ec3a312a5afaf8d410017b1d6576fc4a
Kevin Finch <finchkevinm74@gmail.com>
2017-06-29 11:41:53 -0400

    Correctly remove purchased assets

--- Assets/EasyRoads3D/Editor/TerrainEditorScript.js
+++
@@ -1,9 +0,0 @@
-@CustomEditor(EasyRoads3DTerrainID)
-class TerrainEditorScript extends Editor
-{
-function OnSceneGUI()
-{
-if(Event.current.shift && RoadObjectScript.ODCQQODDCO != null) Selection.activeGameObject = RoadObjectScript.ODCQQODDCO.gameObject;
-else RoadObjectScript.ODCQQODDCO = null;
-}
-}
--- Assets/EasyRoads3D/scripts/EasyRoads3DTerrainID.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class EasyRoads3DTerrainID : MonoBehaviour {
-	public string terrainid;
-}
