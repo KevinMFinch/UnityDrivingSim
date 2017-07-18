2379a8d6c77ff9c9b4b4e17842cf8e48c6684c0f
Kevin Finch <finchkevinm74@gmail.com>
2017-07-18 14:57:35 -0400

    Attempt to add building meshes... fail

--- Assets/Scripts/CreateBuildings.cs
+++ Assets/Scripts/CreateBuildings.cs
@@ -30,17 +30,60 @@ public class CreateBuildings : MonoBehaviour {
 				JSONArray coordArray = building [0].AsArray;
 				// Create the list of vertices for this building (building[i])
 				List<Vector3> vertices = new List<Vector3> ();
+				float height = Random.Range (15, 60);
 				for (int j = 0; j < coordArray.Count; j++) {
 					float xCoord = Calc.latToXCoord (coordArray [j] [1].AsFloat);
 					float zCoord = Calc.longToZCoord (coordArray [j][0].AsFloat);
 					Vector3 loc = new Vector3 (xCoord, 0.0f, zCoord);
+					Vector3 skyLoc = new Vector3 (xCoord, height, zCoord);
 					vertices.Add (loc);
+					vertices.Add (skyLoc);
 				}
 				// Add building[i] to the list of buildings
 				buildingVertices.Add (vertices);
-				Debug.Log(buildingFeatures[i]["properties"]["height"].Value);
-
 			}
+		}
+		createBuildings ();
+	}
+
+	void createBuildings() {
+		for (int i = 0; i < buildingVertices.Count; i++) {
+			GameObject thisBuilding = new GameObject ();
+			thisBuilding.AddComponent<MeshFilter>();
+			thisBuilding.AddComponent<MeshRenderer>();
+			Mesh mesh = new Mesh (); 
+			mesh.Clear();
+
+			Vector3[] vertices = buildingVertices [i].ToArray();
+			mesh.vertices = vertices;
+			Vector2[] uvs = new Vector2[vertices.Length];
+			for (int j = 0; j < uvs.Length; j++)
+			{
+				uvs[j] = new Vector2(vertices[j].x, vertices[j].z);
+			}
+			mesh.uv = uvs;
+
+			int mult3 = nearestMultipleOfThree (vertices.Length);
+			int[] triangles = new int[mult3];
+			for (int j = 0; j < triangles.Length; j++) {
+				if (j <= vertices.Length) {
+					triangles [j] = j;
+				} else {
+					triangles[j] = vertices.Length - j;
+				}
+			}
+			mesh.triangles = triangles;
+			thisBuilding.GetComponent<MeshFilter>().mesh = mesh;
+		}
+	}
+
+	int nearestMultipleOfThree(int input) {
+		if ((input + 1) % 3 == 0) {
+			return input + 1;
+		} else if ((input + 2) % 3 == 0) {
+			return input + 2;
+		} else {
+			return input;
 		}
 	}
 }
