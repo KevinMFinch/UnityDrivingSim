c593add10e2dd0fe8c9adf708acf330d15ab982c
Kevin Finch <finchkevinm74@gmail.com>
2017-07-24 10:51:33 -0400

    Create car + follow cam for pedestrian scene

---
+++ Assets/Scripts/Simulation/OverheadFollowCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadFollowCam : MonoBehaviour {
+
+	public GameObject player;
+	// The player game object
+	private Vector3 offset;
+	// The offset between the camera and the player, keeps relative position constant
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
