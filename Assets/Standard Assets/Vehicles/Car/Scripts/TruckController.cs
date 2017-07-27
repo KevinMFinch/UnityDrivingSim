dd96dee15f3c67adcbb754191954c6c8cf546aa9
Kevin <finchkevinm74@gmail.com>
2017-07-27 13:51:26 -0400

    Get truck to follow waypoints

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/TruckController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour {
+
+	public GameObject waypointObj; // The parent object of the waypoints the truck should follow
+	private Vector3[] waypoints;	// The actual waypoints
+	private int currentWaypoint;	// The current waypoint to navigate towards
+	private float speed = 0.5f;			// The distance to cover in one frame
+
+	// Use this for initialization
+	void Start () {
+		waypoints = new Vector3[3];
+		waypoints [0] = waypointObj.transform.GetChild (0).position;
+		Debug.Log (waypoints [0]);
+		waypoints [1] = waypointObj.transform.GetChild (1).position;
+		Debug.Log (waypoints [1]);
+		waypoints [2] = waypointObj.transform.GetChild (2).position;
+		Debug.Log (waypoints [2]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentWaypoint < waypoints.Length) {
+			Vector3 target = waypoints [currentWaypoint];
+			Vector3 direction = target - gameObject.transform.position;
+			Vector3 unitVector = direction.normalized;
+			Vector3 newPos = gameObject.transform.position += speed * unitVector;
+			Quaternion rotation = gameObject.transform.rotation;
+			gameObject.transform.SetPositionAndRotation (newPos, rotation);
+
+			if (CheckAdvanceWaypoint ()) {
+				currentWaypoint++;
+			}
+
+		}
+	}
+
+	// Checks if the next waypoint should be targeted
+	bool CheckAdvanceWaypoint() {
+		float distance = Vector3.Distance (gameObject.transform.position, waypoints [currentWaypoint]);
+		return distance < 0.5f;
+	}
+		
+}
